336271511525dcb6479ed2f2da49152acccdc3b2
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-26 12:17:16 +0300

    create reusable paragrpah component that receives emoji and content

---
+++ src/components/ParagraphContainer.jsx
@@ -0,0 +1,12 @@
+
+
+function ParagraphContainer({emoji,content}) {
+    return (
+        <p>
+            <span>{emoji}</span>
+            <span>{content}</span>
+        </p>
+    )
+}
+
+export default ParagraphContainer
