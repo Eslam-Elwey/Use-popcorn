5c5d1370e7fd714287d176eee606eac2c6075234
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-29 11:37:51 +0300

    handle selected movie to lift up to app component

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,13 +1,20 @@
 import ParagraphContainer from "./ParagraphContainer";
+import defaultsrc from "../assets/defaultMovie.webp";
 
 
-function SingleMovie({movie}) {
+function SingleMovie({movie,setSelectedMovieID,selectedMovieID}) {
 
     const {imdbID,Title,Year,Poster} = movie ;
 
+    function handleClick()
+    {
+        console.log(imdbID);
+        setSelectedMovieID(()=>imdbID===selectedMovieID?null:imdbID);
+    }
+
     return (
-        <li>
-            <img src={Poster} alt={Title} />
+        <li onClick={()=>handleClick()}>
+            <img src={Poster==="N/A"?defaultsrc:Poster} alt={Title} />
             <h3>{Title}</h3>
             <ParagraphContainer emoji="📆" content={`${Year}`} />
         </li>
