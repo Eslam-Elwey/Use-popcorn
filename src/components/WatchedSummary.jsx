7171f86faa2312becc3554562da9465a1359c728
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-29 11:39:57 +0300

    add to fixed to avergae calculation

--- src/components/WatchedSummary.jsx
+++ src/components/WatchedSummary.jsx
@@ -8,17 +8,18 @@ function WatchedSummary({watchedMovies})
     const average = (arr=[])=>(arr.reduce((acc,curr,ind,arr)=>acc+curr/arr.length,0));
 
     const avergeIMDBRating = average(watchedMovies.map((movie)=>movie.imdbRating)) ;
-    const avergeUserRating = average(watchedMovies.map((movie)=>movie.userRating)) ;
-    const avergeWatchedTime = average(watchedMovies.map((movie)=>movie.runtime)) ;
+    const avergeUserRating = average(watchedMovies.map((movie)=>parseInt(movie.userRating))) ;
+    const avergeWatchedTime = average(watchedMovies.map((movie)=>parseInt(movie.runtime)===NaN?0:parseInt(movie.runtime))) ;
 
     return(
         <div className='summary'>
             <h2>movies you watched</h2>
             <div>
                 <ParagraphContainer emoji="#️⃣" content={`${totalWatchedMovies} movies`} />
-                <ParagraphContainer emoji="⭐" content={`${avergeIMDBRating}`} />
-                <ParagraphContainer emoji="🌟" content={`${avergeUserRating}`} />
-                <ParagraphContainer emoji="⏳" content={`${avergeWatchedTime} min`} />
+                <ParagraphContainer emoji="⭐" content={`${avergeIMDBRating.toFixed(1)}`} />
+                <ParagraphContainer emoji="🌟" content={`${avergeUserRating.toFixed(1)}`} />
+                <ParagraphContainer emoji="⏳" content={`${avergeWatchedTime.toFixed(1)} min`} />
+                
             </div>
         </div>
     );
