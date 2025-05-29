e4e725d72a98135dec530765b4d0a0e6d09a38aa
Eslam-Elwey <eslam.elwey@gmail.com>
2025-05-29 11:41:03 +0300

    call omdb api on mount and on query change and query length bigger than 3 letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import MainSec from "./components/MainSec";
+import Navbar from "./components/Navbar";
+import Movies from "./components/Movies";
+import WatchedMovies from "./components/WatchedMovies";
+import ListBox from "./components/ListBox";
+import Button from "./components/Button";
+import WatchedSummary from "./components/WatchedSummary";
+import Search from "./components/Search";
+import StarRating from "./components/StarRating";
+import MovieFullDetails from "./components/MovieFullDetails";
 
-import { useState } from 'react';
-import './App.css'
-import MainSec from './components/MainSec';
-import Navbar from './components/Navbar';
-import Movies from './components/Movies';
-import WatchedMovies from './components/WatchedMovies';
-import ListBox from './components/ListBox';
-import Button from './components/Button';
-import WatchedSummary from './components/WatchedSummary';
-import Search from './components/Search';
-import StarRating from './components/StarRating';
+export const key = "d8126ba3";
 
 const tempMovieData = [
   {
@@ -58,69 +60,157 @@ const tempWatchedData = [
   },
 ];
 
-function Test()
-{
-  const [movieRating,setMovieRating]  = useState(0); 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
 
   return (
     <>
-      <StarRating color ="blue" setStateFun={setMovieRating} />
+      <StarRating color="blue" setStateFun={setMovieRating} />
       <p>Rating is {movieRating}</p>
     </>
   );
-  
 }
 
+function ErrorMessage({ message }) {
+  return(
+  <p className="error">
+    <span>⛔</span>
+    {message}
+  </p>
+  )
+}
+
+
+
 function App() {
-  const [resultMovies , setResultMovies] = useState(tempMovieData);
-  const [watchedMovies , setWatchedMovies] = useState(tempWatchedData);
-  const[query,setQuery] = useState("");
-  
+  const [resultMovies, setResultMovies] = useState([]);
+  const [watchedMovies, setWatchedMovies] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState("");
+  const [query, setQuery] = useState("inception");
+  const [selectedMovieID, setSelectedMovieID] = useState(null);
 
-  
+  useEffect(()=>console.log(selectedMovieID),[selectedMovieID]);
 
-    function Logo()
-    {
-        return (
-            <div className="logo">
-                <span role="logo">🍿</span>
-                <h1>usePopcorn</h1>
-            </div>
+  function handleAddwatchedMovie(movie)
+  {
+    setWatchedMovies(()=>[...watchedMovies,movie]);
+    handleCloseDetails();
+  }
+
+
+  function removeMovieFromWatchlist(movieID)
+  {
+    setWatchedMovies(()=>watchedMovies.filter((movie)=>movie.imdbID!=movieID));
+  }
+
+  useEffect(function () {
+    setIsloading(() => true);
+    setError("");
+    async function fetchMovies() {
+      try {
+        const response = await fetch(
+          `https://www.omdbapi.com/?apikey=${key}&s=${query}`
         );
+
+        if (!response.ok) {
+          throw new Error("Something went wrong in fetching movies");
+        }
+        const data = await response.json();
+        console.log(data);
+        if (data.Response === "False") {
+          throw new Error("No movies found");
+        }
+        setResultMovies(data.Search);
+        
+      } catch (err) {
+        console.log(err.message);
+        if(err.message==="Failed to fetch")
+        {
+          console.err(err);
+          err.message = "Something went wrong in fetching movies" ;
+        }
+        setError(() => err.message);
+      } finally {
+        setIsloading(() => false);
+      }
     }
 
-    function Results({foundMoviesLength})
+    if(query.length<3)
     {
-        return(<h3 className="num-results">Found <strong>{foundMoviesLength}</strong> results</h3>);
+      setError(()=>"");
+      setResultMovies(()=>[]);
+      setIsloading(false);
+      return ;
     }
+    fetchMovies();
+  }, [query]);
 
-    const messages =['terrible','bad','ok','good','amazing'];
+  function handleCloseDetails()
+  {
+    setSelectedMovieID(()=>null);
+  }
+
+  function Logo() {
+    return (
+      <div className="logo">
+        <span role="logo">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+    );
+  }
+
+  function Results({ foundMoviesLength }) {
+    return (
+      <h3 className="num-results">
+        Found <strong>{foundMoviesLength}</strong> results
+      </h3>
+    );
+  }
+
+  const messages = ["terrible", "bad", "ok", "good", "amazing"];
 
   return (
     <>
-    <Test/>
-    <StarRating maxRating = {5} color='green' size={30} className='test' defaultRating={3} messages={messages}/>
-    <StarRating maxRating = {10} />
+      {/* <Test />
+      <StarRating
+        maxRating={5}
+        color="green"
+        size={30}
+        className="test"
+        defaultRating={3}
+        messages={messages}
+      />
+      <StarRating maxRating={10} /> */}
       <Navbar>
-        <Logo/>
-        <Search query={query} setQuery={setQuery}/>
-        <Results foundMoviesLength={resultMovies.length}/>
+        <Logo />
+        <Search query={query} setQuery={setQuery} />
+        <Results foundMoviesLength={resultMovies.length} />
       </Navbar>
 
       <MainSec>
         {/* {render found movies } */}
         <ListBox>
-          <Movies resultMovies={tempMovieData}/>
+          {isLoading && <p className="loader">Laoding...</p>}
+          {!isLoading && !error && <Movies selectedMovieID={selectedMovieID}  resultMovies={resultMovies}  setSelectedMovieID={setSelectedMovieID}/>}
+          {error && <ErrorMessage message={error} />}
         </ListBox>
 
         {/* {render watched movies } */}
         <ListBox>
-          <WatchedSummary watchedMovies={watchedMovies}></WatchedSummary>
-          <WatchedMovies  watchedMovies={watchedMovies} />
+          {
+            
+            selectedMovieID?<MovieFullDetails watchedMovies={watchedMovies} onAddMovieToList={handleAddwatchedMovie} onCloseDetails={handleCloseDetails} selectedMovieID={selectedMovieID} /> :
+            
+            <>
+              <WatchedSummary watchedMovies={watchedMovies}></WatchedSummary>
+              <WatchedMovies onRemove={removeMovieFromWatchlist} watchedMovies={watchedMovies} />
+            </>
+          }
         </ListBox>
       </MainSec>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
